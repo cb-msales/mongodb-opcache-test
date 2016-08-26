9c745167704419c01de621b25b59d21e66d48d23
Christoph Beyer <christoph.beyer@msales.com>
2016-08-26 17:02:09 +0200

    first version

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$test = ['test' => ['$in' => [10, 12]]];
+
+$client = new MongoDB\Client();
+$client->selectCollection('a','b')->find($test);
+
+file_put_contents('/tmp/mongodb-errors.log', (int)(serialize($test) == 'a:1:{s:4:"test";a:1:{s:3:"$in";a:2:{i:0;i:10;i:1;i:12;}}}'), FILE_APPEND);
